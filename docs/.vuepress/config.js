5df0100f594c467fdd25c37ae32b8def8f8e1e3a
相霄 <xiangxiao@xiangxiaodeMacBook-Pro.local>
2019-08-25 18:16:49 +0800

    change to light

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,7 +35,8 @@ module.exports = {
     repo: "https://github.com/xxholly32/vuepress-plugin-code-box.git",
     editLinks: false,
     lastUpdated: false,
-    defaultTheme: { light: [6, 18], dark: [18, 6] },
+    defaultTheme: "light",
+    // defaultTheme: { light: [6, 18], dark: [18, 6] },
     nav: [
       {
         text: "指南",
